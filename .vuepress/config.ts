35439fc56b599e46607f57c13cda413bb484ffff
郑岩 <18849313639@139.com>
2023-07-28 16:53:03 +0800

    sh0728

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -4,6 +4,7 @@ import recoTheme from "vuepress-theme-reco";
 
 export default defineUserConfig({
   title: "关耳的博客",
+  "base": "/myblog/",
   description: "Just playing around",
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
